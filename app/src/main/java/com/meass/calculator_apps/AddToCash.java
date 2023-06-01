ea75d6e6564d6ed20d9d0ff66f7ee8ea6db1d7cb
Arifuljava <arifulislam.java.c@gmail.com>
2023-06-01 13:53:33 +0600

    main

--- app/src/main/java/com/meass/calculator_apps/AddToCash.java
+++ app/src/main/java/com/meass/calculator_apps/AddToCash.java
@@ -6,6 +6,12 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AddToCash extends AppCompatActivity {
 
@@ -31,7 +37,30 @@ public class AddToCash extends AppCompatActivity {
         getSupportActionBar().setElevation(10.0f);
         getSupportActionBar().setElevation(10.0f);
         getSupportActionBar().setElevation(10.0f);
+
+        somitiname=findViewById(R.id.somitiname);
+        sovapoti=findViewById(R.id.sovapoti);
+        sovapoti_english=findViewById(R.id.sovapoti_english);
+        fatherba=findViewById(R.id.fatherba);
+        fatheren=findViewById(R.id.fatheren);
+        mother=findViewById(R.id.mother);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+
+        Button cirLoginButton=findViewById(R.id.cirLoginButton);
+        cirLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
+    FirebaseFirestore firebaseFirestore;
+    FirebaseAuth firebaseAuth;
+    EditText somitiname,mother,fatheren,fatherba,sovapoti_english,sovapoti;
 
     @Override
     public void onBackPressed() {
